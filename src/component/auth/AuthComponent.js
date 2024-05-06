9db42756d9e918cb993835a64be659c0c74d8bb4
Codetraker <swarajbibhuprasad@gmail.com>
2024-05-07 00:47:32 +0530

    auth page

---
+++ src/component/auth/AuthComponent.js
@@ -0,0 +1,9 @@
+import './AuthComponent.css';
+const AuthComponent = () =>{
+    return (
+        <div className="box">
+            <h1>Sign up</h1>
+        </div>
+    );
+};
+export default AuthComponent; 
---
+++ src/pages/AuthPage.js
@@ -0,0 +1,8 @@
+import AuthComponent from "../component/auth/AuthComponent";
+
+const AuthPage = () =>{
+    return (
+        <AuthComponent />
+    );
+};
+export default AuthPage;
