d92282c47df12e8b8dce4b037becc13b6afbcca0
Codetraker <swarajbibhuprasad@gmail.com>
2024-05-15 03:40:33 +0530

    Add portal for minimize and maximize function

--- src/component/layout/WriteMail/Compose.js
+++ src/component/layout/WriteMail/Compose.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import { IconButton } from '@mui/material';
 import './Compose.css';
 import MinimizeIcon from '@mui/icons-material/Minimize';
@@ -19,56 +20,125 @@ import { useState } from 'react';
 
 
 const Compose = () => {
+    const [maximize, setMaximize] = useState(false);
     const [minimize, setMinimize] = useState(false);
+    
     const minimizeHandler = () =>{
         setMinimize(!minimize);
+        setMaximize(false);
     };
-  return (
-    <>
-    <div className={minimize?'compose_minimize':'compose'}>
-        <div className='header'>
-            <div className='header_left'>
-                <span>New Message</span>
-            </div>
-            <div className='header_right'>
-                <IconButton onClick={minimizeHandler}><MinimizeIcon/></IconButton>
-                <IconButton><OpenInFullIcon/></IconButton>
-                <IconButton><CloseIcon/></IconButton>
-            </div>
-        </div>
-        {!minimize && (
-        <>
-            <div className='body'>
-                <div className='bodyForm'>
-                    <input type='email' placeholder='Recipients'/>
-                    <input type='text' placeholder='Subject'/>
-                    <textarea />
+    const maximizeHandler = () =>{
+        setMinimize(false);
+        setMaximize(!maximize);
+    };
+
+    if(maximize){
+        return (
+            <>
+                {ReactDOM.createPortal(
+                   <>
+                        <div className='backDrop'></div>
+                   </>,
+                   document.getElementById('backdrop-root')
+                )}
+                {ReactDOM.createPortal(
+                    <>
+                       <div className={minimize?'compose_minimize':'compose_maximize'}>
+                            <div className='header'>
+                                <div className='header_left'>
+                                    <span>New Message</span>
+                                </div>
+                                <div className='header_right'>
+                                    <IconButton onClick={minimizeHandler}><MinimizeIcon/></IconButton>
+                                    <IconButton onClick={()=>{setMinimize(false);setMaximize(false)}}><OpenInFullIcon/></IconButton>
+                                    <IconButton><CloseIcon/></IconButton>
+                                </div>
+                            </div>
+                            {!minimize && (
+                            <>
+                                <div className='body'>
+                                    <div className='bodyForm'>
+                                        <input type='email' placeholder='Recipients'/>
+                                        <input type='text' placeholder='Subject'/>
+                                        <textarea />
+                                    </div>
+                                </div>
+                                <div className='footer'>
+                                    <div className='footer_left'>
+                                        <button type='submit' className='send'>
+                                            Send <ArrowDropDownIcon/>
+                                        </button>
+                                        <IconButton><FormatColorTextIcon /></IconButton>
+                                        <IconButton><AttachFileIcon /></IconButton>
+                                        <IconButton><LinkIcon /></IconButton>
+                                        <IconButton><InsertEmoticonIcon /></IconButton>
+                                        <IconButton><AddToDriveIcon /></IconButton>
+                                        <IconButton><ImageOutlinedIcon /></IconButton>
+                                        <IconButton><LockClockOutlinedIcon /></IconButton>
+                                        <IconButton><DriveFileRenameOutlineIcon /></IconButton>
+                                        <IconButton><MoreVertIcon /></IconButton>
+                                    </div>
+                                    <div className='footer_right'>
+                                        <IconButton><DeleteOutlineIcon /></IconButton>
+                                    </div>
+                                </div>
+                            </>
+                            )}
+                        </div> 
+                    </>,
+                    document.getElementById('overlay-root')
+                )}
+            </>
+        );
+    }else{
+        return (
+            <>
+            <div className={minimize?'compose_minimize':'compose'}>
+                <div className='header'>
+                    <div className='header_left'>
+                        <span>New Message</span>
+                    </div>
+                    <div className='header_right'>
+                        <IconButton onClick={minimizeHandler}><MinimizeIcon/></IconButton>
+                        <IconButton onClick={maximizeHandler}><OpenInFullIcon/></IconButton>
+                        <IconButton><CloseIcon/></IconButton>
+                    </div>
                 </div>
+                {!minimize && (
+                <>
+                    <div className='body'>
+                        <div className='bodyForm'>
+                            <input type='email' placeholder='Recipients'/>
+                            <input type='text' placeholder='Subject'/>
+                            <textarea />
+                        </div>
+                    </div>
+                    <div className='footer'>
+                        <div className='footer_left'>
+                            <button type='submit' className='send'>
+                                Send <ArrowDropDownIcon/>
+                            </button>
+                            <IconButton><FormatColorTextIcon /></IconButton>
+                            <IconButton><AttachFileIcon /></IconButton>
+                            <IconButton><LinkIcon /></IconButton>
+                            <IconButton><InsertEmoticonIcon /></IconButton>
+                            <IconButton><AddToDriveIcon /></IconButton>
+                            <IconButton><ImageOutlinedIcon /></IconButton>
+                            <IconButton><LockClockOutlinedIcon /></IconButton>
+                            <IconButton><DriveFileRenameOutlineIcon /></IconButton>
+                            <IconButton><MoreVertIcon /></IconButton>
+                        </div>
+                        <div className='footer_right'>
+                            <IconButton><DeleteOutlineIcon /></IconButton>
+                        </div>
+                    </div>
+                </>
+                )}
             </div>
-            <div className='footer'>
-                <div className='footer_left'>
-                    <button type='submit' className='send'>
-                        Send <ArrowDropDownIcon/>
-                    </button>
-                    <IconButton><FormatColorTextIcon /></IconButton>
-                    <IconButton><AttachFileIcon /></IconButton>
-                    <IconButton><LinkIcon /></IconButton>
-                    <IconButton><InsertEmoticonIcon /></IconButton>
-                    <IconButton><AddToDriveIcon /></IconButton>
-                    <IconButton><ImageOutlinedIcon /></IconButton>
-                    <IconButton><LockClockOutlinedIcon /></IconButton>
-                    <IconButton><DriveFileRenameOutlineIcon /></IconButton>
-                    <IconButton><MoreVertIcon /></IconButton>
-                </div>
-                <div className='footer_right'>
-                    <IconButton><DeleteOutlineIcon /></IconButton>
-                </div>
-            </div>
-        </>
-        )}
-    </div>
-    </>
-  )
+            </>
+          )
+    };
+  
 };
 
 export default Compose;
