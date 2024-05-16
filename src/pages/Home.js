1f0b15dab4a96cd046bdaada60c5da10136c51de
Codetraker <swarajbibhuprasad@gmail.com>
2024-05-17 03:02:59 +0530

    include realtime snapshot display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 import NavBar from "../component/layout/NavBar/NavBar";
 import SideBar from "../component/layout/SideBar/SideBar";
 import './Home.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TopBarMailBox from "../component/layout/MailInbox/TopBarMailBox";
 import InboxIcon from '@mui/icons-material/Inbox';
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
@@ -11,7 +11,7 @@ import MailList from "../component/layout/MailInbox/MailList";
 import MailSection from "../component/layout/MailInbox/MailSection";
 import Compose from "../component/layout/WriteMail/Compose";
 import { useSelector } from "react-redux";
-import { composerDisplayAction } from "../store/composerDisplaySlice";
+import { db } from "../firebase";
 
 const Home = () =>{
     const isVisible = useSelector((state)=>state.composerVisible.composerIsVisible);
@@ -20,6 +20,16 @@ const Home = () =>{
     const sidebarDisplayHandler = () =>{
         setDisplaySidebar(!displaySidebar);
     };
+    const [emails, setEmails] = useState([]);
+    useEffect(()=>{
+        db.collection("emails").orderBy("timestamp","desc").onSnapshot(snapshot=>{
+            setEmails(snapshot.docs.map(doc=>({
+                id:doc.id,
+                data:doc.data()
+            })))
+        })
+    },[])
+    console.log(emails);
     return (
         <>
             <NavBar onDisplaySide={sidebarDisplayHandler}/>
@@ -39,8 +49,11 @@ const Home = () =>{
                             <MailSection Icon={PeopleAltOutlinedIcon} text="Social"/>
                         </div>
                         <div className="mailInbox">
-                            <MailList name="IBM Careers" subject="Review your CV" message="This is a demo related to CV" time="10:07AM 14 May"/>
-                            
+                            {
+                                emails.map(({id,data})=>{
+                                    return <MailList key={id} name={data.to} subject={data.subject} message={data.message} time={new Date(data.timestamp?.seconds*1000).toLocaleTimeString()}/>
+                                })
+                            }
                         </div>
                     </div>
                 </Col>
